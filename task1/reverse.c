1d22c54917f9c045c41528da82e85fbf3bc8a531
TimerChen <TimeMachine@sjtu.edu.cn>
2018-03-14 16:32:47 +0800

    task1 finished

--- task1/reverse.c
+++ task1/reverse.c
@@ -5,12 +5,24 @@
 #include <sys/reg.h>
 #include <sys/syscall.h>
 
+#include <sys/user.h>
+#include <string.h>
+int min(int a,int b){return a<b?a:b;}
+
 const int long_size = sizeof(long);//字长 若本机是64位，则字长应为8字节long
 
 /**
 * TODO 反转str指针指向的字符串
 **/
 void reverse(char *str){
+		int len = strlen(str);
+		char ch;
+		for(int i=0; i<len-1-i; ++i)
+		{
+			ch = str[i];
+			str[i] = str[len-1-i];
+			str[len-1-i] = ch;
+		}
 }
 
 /**
@@ -20,6 +32,23 @@ void reverse(char *str){
 * 你可能会用到：memcpy函数
 **/
 void getdata(pid_t child, long addr, char *str, int len){
+	int i,j;
+	i = 0;
+	union u
+	{
+		long val;
+		char str[long_size];
+	}data;
+	while(i<len)
+	{
+		j = min(long_size, len-i);
+		data.val = ptrace(PTRACE_PEEKDATA, child, addr, NULL);
+		memcpy(str, data.str, j);
+		i += j;
+		addr += j;
+		str += j;
+	}
+	str[i] = 0;
 }
 
 /**
@@ -27,12 +56,28 @@ void getdata(pid_t child, long addr, char *str, int len){
  * 使用 ptrace 的 PTRACE_POKEDATA 来写，需要注意的是由于64位机器的字长是8byte。
  * */
 void putdata(pid_t child, long addr, char *str, int len){
-
+	int i,j;
+	i = 0;
+	union u
+	{
+		long val;
+		char str[long_size];
+	}data;
+	while(i<len)
+	{
+		j = min(long_size, len-i);
+		memcpy(data.str, str, j);
+		ptrace(PTRACE_POKEDATA, child, addr, data.val);
+		i += j;
+		addr += j;
+		str += j;
+	}
 }
 
 int main(){
   pid_t child;
   child = fork();
+  struct user_regs_struct regs;
   if (child < 0) {
     printf("fork error");
   } else if(child == 0) {
@@ -46,6 +91,7 @@ int main(){
     int status;
     char *str, *laddr;
     int toggle = 0;
+
     while(1) {
       //等待子进程信号
       wait(&status);
@@ -53,7 +99,9 @@ int main(){
           break;
       // 使用 PTRACE_PEEKUSER 来获取系统调用号。
       orig_rax = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
+
       if(orig_rax == SYS_write) {
+
         if(toggle == 0) {
           toggle = 1;
           /**
@@ -62,7 +110,14 @@ int main(){
           * 需要注意的是，RAX的宏定义是 ORIG_RAX，
           * 而 RDI RSI RDX 的宏定义为 RDI RSI RDX
           **/
+
+		  ptrace(PTRACE_GETREGS, child, NULL, &regs);
+		  params[0] = regs.rdi;
+		  params[1] = regs.rsi;
+		  params[2] = regs.rdx;
+		  //printf("calloc%d %d %d...\n",(int)params[0], (int)params[1], (int)params[2]);
           str = (char *)calloc((params[2]+1), sizeof(char));
+
           getdata(child, params[1], str,
                   params[2]);
           reverse(str);
@@ -75,6 +130,7 @@ int main(){
     /**
     * TODO 使用 PTRACE_SYSCALL 来让子进程进行系统调用。
     **/
+	ptrace(PTRACE_SYSCALL, child, NULL, NULL);
     }
   }
   return 0;
